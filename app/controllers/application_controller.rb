358dfcdd2a428c275f7f334127ccab8361651297
ltwheeler <ltwheeler@ucdavis.edu>
2012-06-19 14:24:29 -0700

    added scaffolding for CAS authentication and declarative authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter CASClient::Frameworks::Rails::Filter
   protect_from_forgery
+
+	def current_user
+		Person.new
+	end  
+
+	#def set_current_user
+	#	Authorization.current_user = session[:cas_user]
+		#session[:cas_user]
+	#end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+require "casclient"
+require "casclient/frameworks/rails/filter"
+
 # Initialize the rails application
 DssScheduler::Application.initialize!
+CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "https://cas.ucdavis.edu/cas/")
