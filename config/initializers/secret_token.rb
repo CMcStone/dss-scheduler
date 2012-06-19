cfb9254e26fd400bba6f985ffe6ba0fe7e01b8af
Christopher Thielen <cthielen@gmail.com>
2012-06-19 09:45:53 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DssScheduler::Application.config.secret_token = '0f0a8657c096ddf1f10455557f0575f65f4e5da0acc2df7c7ba2f62bd00cf89f41ed4fffdaffe65742ee6627fd8741b040935f418437a4f8e560de35d6b4ec71'
