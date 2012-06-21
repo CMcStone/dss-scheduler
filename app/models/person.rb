362e8435c06658b6fa59d367e86105ceb58aa378
Christopher Thielen <cthielen@gmail.com>
2012-06-21 14:40:29 -0700

    Forgot to update some modelling

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,8 @@
-class Person
+class Person < ActiveRecord::Base
   # attr_accessible :title, :body
   def role_symbols
     [:admin] #return symbols indicating roles
   end
+  
+  has_many :reservations
 end
