2ba76f226722a7bfee4cf1ce4c8f0a6c790741a1
ltwheeler <ltwheeler@ucdavis.edu>
2012-06-20 18:26:42 -0700

    rewrote authorization.current_user assignment to follow example code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   before_filter CASClient::Frameworks::Rails::Filter
-  before_filter {|contr| Authorization.current_user = contr.current_user}
-
-  #before_filter :set_current_user
+  before_filter :set_current_user
+  
   protect_from_forgery
-#	def set_current_user
-#		Authorization.current_user = session[:cas_user]
-#		session[:cas_user]
-#	end
+	def set_current_user
+		Authorization.current_user = session[:cas_user]
+		session[:cas_user]
+	end
 	
 	def current_user
 		Person.new
