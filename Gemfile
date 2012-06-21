740b584e0527d4625896a7fe97ea8050937fd7bb
ltwheeler <ltwheeler@ucdavis.edu>
2012-06-20 18:12:11 -0700

    removed authlogic in gemfile and set Authorization.current_user for model-level security

--- Gemfile
+++ Gemfile
@@ -7,8 +7,7 @@ gem 'rails', '3.2.5'
 
 gem 'rubycas-client'
 gem 'sqlite3'
-#gem 'authlogic', :sourc
-
+gem 'declarative_authorization'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
   before_filter CASClient::Frameworks::Rails::Filter
-  protect_from_forgery
+  before_filter {|contr| Authorization.current_user = contr.current_user}
 
+  #before_filter :set_current_user
+  protect_from_forgery
+#	def set_current_user
+#		Authorization.current_user = session[:cas_user]
+#		session[:cas_user]
+#	end
+	
 	def current_user
 		Person.new
 	end  
-
-	#def set_current_user
-	#	Authorization.current_user = session[:cas_user]
-		#session[:cas_user]
-	#end
 end
