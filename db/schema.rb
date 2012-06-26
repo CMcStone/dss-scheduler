6b564e8980677ba130542b03fef2ac733c246b8e
Christopher Thielen <cthielen@gmail.com>
2012-06-26 14:58:02 -0700

    Added title and description to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120626213943) do
+ActiveRecord::Schema.define(:version => 20120626215455) do
 
   create_table "people", :force => true do |t|
     t.datetime "created_at",  :null => false
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(:version => 20120626213943) do
     t.integer  "package_id"
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
+    t.string   "title"
+    t.text     "description"
   end
 
   create_table "resource_categories", :force => true do |t|
