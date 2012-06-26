c58f01b8277ede467789492a0ebeb18ff173b5a9
Christopher Thielen <cthielen@gmail.com>
2012-06-26 14:21:35 -0700

    Added instructions to resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120621221346) do
+ActiveRecord::Schema.define(:version => 20120626212031) do
 
   create_table "people", :force => true do |t|
     t.datetime "created_at",  :null => false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20120621221346) do
     t.boolean  "allow_anonymous"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.text     "instructions"
   end
 
   create_table "roles", :force => true do |t|
