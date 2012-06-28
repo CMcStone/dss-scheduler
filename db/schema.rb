2b929fa931fde5ba47c6c6c53937b07ec79731ef
Christopher Thielen <cthielen@gmail.com>
2012-06-28 10:46:58 -0700

    Add pool boolean to resource category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120626215455) do
+ActiveRecord::Schema.define(:version => 20120628174608) do
 
   create_table "people", :force => true do |t|
     t.datetime "created_at",  :null => false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(:version => 20120626215455) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "is_pool"
   end
 
   create_table "resource_periods", :force => true do |t|
