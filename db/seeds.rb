9542a3a9f7779bda60767fe833d0672b2348bc73
Carol McStone <cbeck@ucdavis.edu>
2012-07-05 22:33:48 -0700

    added seed data for reservations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+[
+    { :person_id => "2", :cache_start_time => "2012/07/03:14:00:00", :cache_end_time => "2012/07/03:15:00:00", :title => "First Appt"},
+    { :person_id => "5", :cache_start_time => "2012/07/05:11:00:00", :cache_end_time => "2012/07/03:12:00:00", :title => "Second Appt"}
+].each do |reservation|
+    Reservation.find_or_create_by_person_id(reservation)
+end
