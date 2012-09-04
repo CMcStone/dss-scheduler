0e4a171b1239ef1c34f8f246472c870381ad4dd8
Obada Kadri <okadri@ucdavis.edu>
2012-09-04 15:35:27 -0700

    changed wizard method to render without layout

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -92,12 +92,12 @@ class ReservationsController < ApplicationController
     session[:question_response_params].deep_merge!(params[:question_response]) if params[:question_response]
     
     @reservation.current_step = session[:reservation_step]
-    @person = Person.find_by_uid(1) #TODO: Change the 0 to the current user UID after implementing the Roles Mgmt
+    @person = Person.find_by_uid(1) #TODO: Change the 1 to the current user UID after implementing the Roles Mgmt
     @reservations = @person.reservations
     @resource_categories = ResourceCategory.all
-    @departments = Resource.select("DISTINCT(ou_uid), description")
 
-    #debugger
+    # We can have a switch-case statement here to get the variables depending on the step #
+    @departments = Resource.select("DISTINCT(ou_uid), description") 
     @resource = @reservation.resource
     @resource_category = @resource.resource_category if @resource
     @resources = @resource_category.resources if @resource_category
@@ -113,7 +113,7 @@ class ReservationsController < ApplicationController
     if params[:back_button]
       @reservation.previous_step
       session[:reservation_step] = @reservation.current_step
-    elsif true
+    else
       if @reservation.last_step?
         @reservation.save if @reservation.all_valid?
       else
@@ -131,5 +131,7 @@ class ReservationsController < ApplicationController
       flash[:notice] = "Reservation created!"
       redirect_to @reservation
     end
+    
+    #format.html { render :layout => false }
   end
 end
